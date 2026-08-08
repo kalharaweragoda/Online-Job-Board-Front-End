66716c536c3a683879349ca26c3cb899902841b1
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-08 22:29:35 +0530

    create route

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/Auth'
 import Dashboard from './components/Dashboard'
+import Login from './components/Login'
 import './App.css'
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Dashboard />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
