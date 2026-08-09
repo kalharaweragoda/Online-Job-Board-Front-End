08796ee6576f034fca284cb5729767fe0ec3076f
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-09 22:32:07 +0530

    create app route

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { AuthProvider } from './context/Auth'
 import Dashboard from './components/Dashboard'
 import Login from './components/Login'
+import Register from './components/Register'
+import JobCreate from './components/JobCreate'
 import './App.css'
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/jobs/new" element={<JobCreate />} />
         </Routes>
       </BrowserRouter>
     </AuthProvider>
