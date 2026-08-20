beccb1e744a2e42b4792b09cf3c5b65aa56ca4e3
kalhara weragoda <kalharaweragoda@gmail.com>
2026-08-20 10:19:08 +0530

    add js file

--- src/services/client.js
+++ src/services/client.js
@@ -6,7 +6,6 @@ const client = axios.create({
   baseURL: API_BASE,
 });
 
-// Attach access token to every request if present
 client.interceptors.request.use((config) => {
   const token = localStorage.getItem('access_token');
   if (token) {
